eace324f96e02fbb001ee3a11083cf86c605417e
Yoo-Jin Lee <yj@mokten.com>
2021-01-25 22:51:43 +1100

    Added Example Project Cocoapods dependency

---
+++ Examples/Cocoapods-iOS/Source/Helpers/NiblessViewController.swift
@@ -0,0 +1,27 @@
+//
+//  NiblessViewController.swift
+//  Example-iOS
+//
+//  Created by Yoo-Jin Lee on 6/6/19.
+//  Copyright © 2019 Mokten Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+open class NiblessViewController: UIViewController {
+    
+    // MARK: - Methods
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable, message: "Loading this view controller from a nib is unsupported in favor of initializer dependency injection.")
+    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    @available(*, unavailable, message: "Loading this view controller from a nib is unsupported in favor of initializer dependency injection.")
+    public required init?(coder aDecoder: NSCoder) {
+        fatalError("Loading this view controller from a nib is unsupported in favor of initializer dependency injection.")
+    }
+}
---
+++ Examples/Cocoapods-iOS/Source/ViewControllers/Image/ImageViewController.swift
@@ -0,0 +1,79 @@
+//
+//
+//  ViewController.swift
+//
+//  Copyright © 2019 Mokten Pty Ltd. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+import UIKit
+import StubPlay
+
+class ImageViewController: NiblessViewController {
+
+    lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+
+    private let session = URLSession(configuration: URLSessionConfiguration.default)
+    
+    override init() {
+        super.init()
+        self.title = "Image"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubview(imageView)
+        testImageRequest()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        imageView.frame = CGRect(x: 20, y: 100, width: view.frame.width - 40, height: 300)
+    }
+
+    func testImageRequest() {
+        let url = URL(string: "https://www.google.com.au/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+
+        let task = session.dataTask(with: url!) { [weak self] data, response, error in
+            guard let self = self else { return }
+
+            if let error = error {
+                fatalError("ViewController \(error)")
+            }
+            
+            guard let data = data else {
+                fatalError("ViewController no data")
+            }
+
+            DispatchQueue.main.async {
+                self.imageView.image = UIImage(data: data)
+            }
+        }
+
+        task.resume()
+    } 
+
+}
+
